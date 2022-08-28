cea0a58b615f756b127f9a87d362b7e05fd8ce42
Tunax3 <maxi.glaetzner2000@gmail.com>
2022-08-28 03:27:58 +0200

    Aufgabe 3d vollständig. Maybe fertige 3e und angefangene 3f.

--- src/StreamingJava.java
+++ src/StreamingJava.java
@@ -1,11 +1,24 @@
 import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import static java.lang.Byte.parseByte;
+import static java.lang.Double.parseDouble;
+
 public class StreamingJava {
+
+    public static void main (String[]args) throws IOException {
+        String[] str = new String[]{"2012-04-19","2012-03-19","2012-04-19","31","3.2","12.06","11.29","4.37","10.76","2.12","","3.97","44.57",
+                "2012-05-17","2012-04-19","2012-05-17","28","3.4","10.89","11.99","4.64","10.12","2.09","","4.22","43.95",
+                "2012-06-18","2012-05-17","2012-06-18","32","0.9","12.45","3.09","1.23","2.68","1.03","","1.12","21.60"};
+
+        orderByInvoiceDateDesc(Arrays.stream(str));
+    }
     // Aufgabe 2) a)
     public static <E> Stream<E> flatStreamOf(List<List<E>> list) {
         // TODO
@@ -66,7 +79,7 @@ public class StreamingJava {
     // Aufgabe 3) b)
     public static double averageCost(Stream<String> lines) {
         // TODO
-        return lines.mapToDouble(s -> Double.parseDouble
+        return lines.mapToDouble(s -> parseDouble
                         (s.split(",")[12]))
                 .average()
                 .getAsDouble();
@@ -84,32 +97,91 @@ public class StreamingJava {
     // TODO:
     //  1. Create record "NaturalGasBilling".
     //  2. Implement static method: "Stream<NaturalGasBilling> orderByInvoiceDateDesc(Stream<String> stream)".
-    public record NaturalGasBilling (Date Invoice,Date From,Date To,double Billing ,double BilledGJ,double BasicCharge,
+
+    public Date Invoice;
+    public Date From;
+    public Date To;
+    public double Billing;
+    public double BilledGJ;
+    public double BasicCharge;
+    public double DeliveryCharges;
+    public double StorageAndTransport;
+    public double CommodityCharges;
+    public double Tax;
+    public String CleanEnergyLevy;
+    public double CarbonTax;
+    public double Amount;
+
+    static SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
+    public record NaturalGasBilling(Date Invoice,Date From,Date To,double Billing ,double BilledGJ,double BasicCharge,
                                      double DeliveryCharges,double StorageAndTransport,double CommodityCharges,
-                                     double Tax,double CleanEnergyLevy,
+                                     double Tax,String CleanEnergyLevy,
                                      double CarbonTax,double Amount) {
+        public NaturalGasBilling (String[] strings) throws ParseException {
 
-   /**     public NaturalGasBilling(String[] strings) {
-            this {
-                format.parse(strings[0]);
-
-            }
-        } **/
+            this (
+                    formatter.parse(strings[0]),
+                    formatter.parse(strings[1]),
+                    formatter.parse(strings[2]),
+                    parseDouble(strings[3]),
+                    parseDouble(strings[4]),
+                    parseDouble(strings[5]),
+                    parseDouble(strings[6]),
+                    parseDouble(strings[7]),
+                    parseDouble(strings[8]),
+                    parseDouble(strings[9]),
+                    (strings[10]),
+                    parseDouble(strings[11]),
+                    parseDouble(strings[12])
+            );
+        }
     }
 
 
     public static Stream<NaturalGasBilling> orderByInvoiceDateDesc(Stream<String> stream){
-        Stream <NaturalGasBilling> str = Stream.empty();
-        //var r = stream.map(it -> it.split(",")).map(it -> new NaturalGasBilling(it));
-
-        return str;
+        return stream.map(it -> it.split(",",12))
+                .map(it -> {
+                    try {
+                        return new NaturalGasBilling(it);
+                    } catch (ParseException e) {
+                        throw new RuntimeException(e);
+                    }
+                }).sorted(Comparator.comparing(NaturalGasBilling :: Invoice).reversed());
     }
 
     // Aufgabe 3) e)
     // TODO: Implement object method: "Stream<Byte> toBytes()" for record "NaturalGasBilling".
+    public Stream<Byte> toBytes() throws IOException {
+        return fileLines("FIles/NaturalGasBilling.csv")
+                .sequential()
+                .map(it -> parseByte(it));
+    }
 
     // Aufgabe 3) f)
     // TODO: Implement static method: "Stream<Byte> serialize(Stream<NaturalGasBilling> stream)".
+
+  public static Stream<Byte> serialize(Stream<NaturalGasBilling> stream) throws IOException {
+        Stream<Byte> res = orderByInvoiceDateDesc(stream.map(NaturalGasBilling :: toString))
+                .map(NaturalGasBilling :: toString)
+                .map(it -> parseByte(it));
+
+        List <Byte> by  = new ArrayList<>();
+        res.map(it -> by.add(it)).skip(3);
+        System.out.println(Arrays.deepToString(by.toArray()));
+
+        File sample = new File("FIles/sample.csv");
+        PrintWriter writing = new PrintWriter(sample);
+
+        writing.write("Invoice Date,From Date,To Date,Billing Days," +
+                "Billed GJ,Basic charge,Delivery charges,Storage and transport,Commodity charges," +
+                "Tax,Clean energy levy,Carbon tax,Amount");
+
+
+        writing.close();
+
+        return res;
+    }
 
     // Aufgabe 3) g)
     // TODO: Implement static method: "Stream<NaturalGasBilling> deserialize(Stream<Byte> stream)".
